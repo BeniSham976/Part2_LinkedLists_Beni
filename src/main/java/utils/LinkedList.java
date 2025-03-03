6740b50bc10eadf23578fd82209f3c372e9e258b
Beni Shamavu <d00253976@student.dkit.ie>
2025-03-03 23:23:57 +0000

    indexOf(), add(), tail() methods and all Javadocs completed for LinkedList.java

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -2,7 +2,7 @@ package utils;
 
 /**
  *
- * @author michelle
+ * @author Beni
  */
 public class LinkedList {
     private int numElements;
@@ -17,10 +17,18 @@ public class LinkedList {
         this.numElements = 0;
     }
 
+    /**
+     * Returns the number of elements present in the list
+     * @return The number of elements present in the list
+     */
     public int size(){
         return numElements;
     }
 
+    /**
+     * @param pos Represents the position of the song to be found
+     * @return An integer to indicate the position of where the song can be found
+     */
     public String get(int pos){
         //Validate
         if(pos < 0 || pos >= numElements){
@@ -33,8 +41,40 @@ public class LinkedList {
         return current.data;
     }
 
+    /**
+     * @param Song the Song object that will be found by the index of its position on the list
+     * @return the song object found at the specified position
+     */
+    public int indexOf(Object Song){
+        //Validate
+        if (Song == null){
+            throw new IllegalArgumentException("Song list cannot handle null vales");
+        }
+        Field current = first;
+        for (int i = 0; i < numElements; i++) {
+            if (Song.equals(current.data)){
+                return i;
+            }
+            current = current.next;
+        }
+        //Else if no match was found, return -1
+        return -1;
+    }
+
+    /**
+     * Return true if the list has no elements inside
+     * @return True if the list has no elements, otherwise it's false
+     */
     public boolean isEmpty(){
         return numElements == 0;
+    }
+
+    /**
+     * Returns the last element inside the list
+     * @return The last element inside the list
+     */
+    public Field tail(){
+        return last;
     }
 
     private static class Field{
